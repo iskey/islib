f3fb328f664cc0e55ba05ab6fac6e0abaaeae6e5
xieyc <xie.yichao@zte.com.cn>
2016-11-22 19:18:41 +0800

    add leetcode/longest_substring_without_repeating_characters.

---
+++ src/leetcode/longest_substring_without_repeating_characters/solution.c
@@ -0,0 +1,62 @@
+#include "string.h"
+#include "stdlib.h"
+
+#define max(a, b) (((a)> (b))? (a): (b))
+
+int lengthOfLongestSubstring(char *str)
+{
+    //longest substring
+    int ans = 0;
+    //The left position of the illegal string
+    int left = 0;
+    int last[255];
+    memset(last, -1, sizeof(last));
+
+    int i = 0;
+    for(i = 0; i < strlen(str); i++){
+        if(last[str[i]] >= left) left = last[str[i]] + 1;
+        last[str[i]] = i;
+        ans = max(ans, i - left + 1);
+    }
+
+    return ans;
+}
+
+char *longestSubstring(char *str)
+{
+    //longest substring
+    int ans = 0;
+    //The left position of the illegal string
+    int left = 0;
+    //The max left
+    int max_left = 0;
+    int max_right = 0;
+    int last[255];
+    memset(last, -1, sizeof(last));
+
+    int i = 0;
+    for(i = 0; i < strlen(str); i++){
+        if(last[str[i]] >= left) left = last[str[i]] + 1;
+        last[str[i]] = i;
+        if((i - left + 1) > ans){
+            max_left = left;
+            max_right = i;
+            ans = i - left + 1;
+        }
+    }
+
+    //printf("max_left = %d , max_right = %d.\n", max_left, max_right);
+
+    if (max_right <strlen(str))
+        str[max_right + 1] = 0;
+
+    return str + max_left;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("The Length of longest substring is %d\n", lengthOfLongestSubstring(argv[1]));
+    printf("The longest substring is %s\n", longestSubstring(argv[1]));
+
+    return 0;
+}
